77a63f40d140c69d5fc11f2ef022bd2797511585
Aurelie Lerouxel <malkiniou@gmail.com>
2020-11-07 10:21:10 +0100

    retrieve the article by its id

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use App\Entity\Subject;
 // use App\DataFixtures\AppFixtures;
 
@@ -21,17 +22,40 @@ class BlogController extends AbstractController
         $subjects = $subjectRepository->findAll();
 
         return $this->render('blog/index.html.twig', [
-            'subjects' => $subjects,
+            'subjects' => $subjects
         ]);
     }
 
     /**
-     * @Route("/article", name="article")
+     * @Route("/article/{id}", name="article", requirements={"id"="\d+"}, defaults={"id": 1})
      */
-    public function article(): Response
+    public function article(int $id): Response
     {
+        $subjectRepository = $this->getDoctrine()->getRepository(Subject::class);
+
+        $article = $subjectRepository->find($id);
+
         return $this->render('blog/article.html.twig', [
-            // 'controller_name' => 'BlogController',
+            'article' => $article
         ]);
     }
+
+    /**
+     * @Route("/edit", name="edit")
+     */
+    public function edit($id)
+    {
+    	return $this->render('blog/edit.html.twig', [
+         
+        ]);
+    }
+
+    /**
+     * @Route("/delete", name="delete")
+     */
+    public function delete($id)
+    {
+    	return new Response('<h1>Delete article: ' .$id. '</h1>');
+    }
+  
 }
